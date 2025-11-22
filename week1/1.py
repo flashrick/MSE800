e9f574a3c7c87997d815b43901d23407ecf34e66
Wade Wang <flashrick2019@gmail.com>
2025-11-23 08:48:58 +1300

    week1

---
+++ week1/1.py
@@ -0,0 +1,26 @@
+# Build a Multi-Type Profile
+
+# Name
+name = "Tianxiang Wang"
+
+# Age
+age = 35
+
+# Skills
+skills = ["Python", "MySQL"]
+
+# Education
+education = ("BSc Computer Science", 2014)
+
+# Contact Details
+contact_details = {"email": "xxxx1111@gmail.com", "phone": "02040111111"}
+
+# Certifications
+certifications = {"Azure", "AWS", "Aliyun"}
+
+print("Name:", name)
+print("Age:", age)
+print("Skills:", skills)
+print("Education:", education)
+print("Contact Details:", contact_details)
+print("Certifications:", certifications)
